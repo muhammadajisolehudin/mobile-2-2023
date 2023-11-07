50bcd62762bd3c2fb4a7bf89ad15edf6fb19826b
mobile-2 <labtif.unsur@gmail.com>
2023-11-07 11:01:27 +0700

    pertemuan 6

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:wisata_app/detail_screen.dart';
+import 'package:wisata_app/Screens/detail_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
